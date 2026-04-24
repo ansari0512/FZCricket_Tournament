acbc3127468d6a2f7bb7c11d39f67ed68e679d33
Shahid Ansari <shahidansari0512@gmail.com>
2026-04-24 18:08:08 +0530

    fix: remove Vite default unused CSS, fix App.css import, remove unused assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import './App.css'
 import { AppProvider } from './context/AppContext'
 import Navbar from './components/Navbar'
 import BottomNav from './components/BottomNav'
